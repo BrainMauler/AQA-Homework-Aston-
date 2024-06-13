e46f0b47b1ecaabfc9acc4f425a6f79a538d2939
Кирилл Миронов <kir-mir@inbox.ru>
2024-06-13 21:40:50 +0400

    Delete Lesson5.java

--- Lesson5.java
+++
@@ -1,8 +0,0 @@
-public class Lesson5 {
-    public static void main(String[] args) {
-        printThreeWords();
-    }
-    public static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple");
-    }
-}
