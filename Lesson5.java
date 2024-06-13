d5e42882225117efe385800f7a637428737e1932
Кирилл Миронов <kir-mir@inbox.ru>
2024-06-13 21:34:51 +0400

    Add files via upload

---
+++ Lesson5.java
@@ -0,0 +1,8 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+}
